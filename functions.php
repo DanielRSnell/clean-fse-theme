b4c2fc1fcc27ac18246c8ff9fbf9b74cb6f6225b
Artisan <daniel@domartisan.com>
2025-01-30 02:09:42 -0500

    update

--- functions.php
+++ functions.php
@@ -14,3 +14,33 @@ add_action('after_setup_theme', function() {
 
 // Include Carbon Fields block definitions
 require_once __DIR__ . '/inc/blocks.php';
+
+// Function to get compiled stylesheet details
+function get_compiled_stylesheet() {
+    $compiled_stylesheet = get_template_directory() . '/compile/tailwind.css';
+    if (file_exists($compiled_stylesheet)) {
+        return array(
+            'url' => get_template_directory_uri() . '/compile/tailwind.css',
+            'version' => filemtime($compiled_stylesheet)
+        );
+    }
+    return false;
+}
+
+// Enqueue compiled stylesheet for frontend
+function enqueue_compiled_stylesheet_frontend() {
+    $stylesheet = get_compiled_stylesheet();
+    if ($stylesheet) {
+        wp_enqueue_style('compiled-styles', $stylesheet['url'], array(), $stylesheet['version']);
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_compiled_stylesheet_frontend');
+
+// Enqueue compiled stylesheet for block editor
+function enqueue_compiled_stylesheet_editor() {
+    $stylesheet = get_compiled_stylesheet();
+    if ($stylesheet) {
+        wp_enqueue_style('compiled-styles-editor', $stylesheet['url'], array(), $stylesheet['version']);
+    }
+}
+add_action('enqueue_block_editor_assets', 'enqueue_compiled_stylesheet_editor');
--- src/blocks/accordion/block.php
+++ src/blocks/accordion/block.php
@@ -4,10 +4,13 @@ use Carbon_Fields\Field;
 
 Block::make('accordion')
     ->add_fields(array(
+        Field::make('text', 'accordion_name', 'Accordion Name')
+            ->set_help_text('A unique name for this accordion group'),
         Field::make('complex', 'accordion_items', 'Accordion Items')
             ->add_fields(array(
                 Field::make('text', 'title', 'Title'),
                 Field::make('rich_text', 'content', 'Content'),
+                Field::make('checkbox', 'default_open', 'Open by default')
             ))
     ))
     ->set_render_callback(function($fields, $attributes, $inner_blocks) {
